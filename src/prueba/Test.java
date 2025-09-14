90e876afc26d1e210f9cd7e3773e3ee037df50d9
Valentín <valenipuente@hotmail.com>
2025-09-14 15:46:49 -0300

    Segundo Commit

--- src/prueba/Test.java
+++ src/prueba/Test.java
@@ -3,6 +3,11 @@ package prueba;
 public class Test {
 		public static void main(String[] args) {
 		System.out.println("Hola, mundo!");
+		
+		System.out.println("Modificación en Eclipse, probando un nuevo commit"); 
+		// probando probando 
+		
+		
 	}
 
 }
